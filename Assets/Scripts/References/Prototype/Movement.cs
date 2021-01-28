bc2467c2afc15850c9d687091bccc97ec0052e0e
Tyler Kortge <tylerkortge@charter.net>
2021-01-28 11:24:58 -0500

    Built a scene with custom lighting.

---
+++ Assets/Scripts/References/Prototype/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+    Vector3 velocity;
+    public float gravity = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
