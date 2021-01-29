f9ef552b55b22060669a862fafd7f8c611e82a18
Tyler Kortge <tylerkortge@charter.net>
2021-01-29 18:39:04 -0500

    Added icons for the items in the inventory.

--- Assets/Scripts/Narration.cs
+++ Assets/Scripts/Narration.cs
@@ -18,31 +18,31 @@ public class Narration : MonoBehaviour
         
     }
 
-    public void InventoryDescription(int itemID)
+    public void InventoryDescription(string subject)
     {
         description = GetComponent<Text>();
-        print("Works");
-        switch (itemID)
+        switch (subject)
         {
-            case 0:
+            // Inventory Items
+            case observe:
                 description.text = "Allows me to take a closer look at whatever got trapped inside my couch, although I was quite content with not knowing!";
                 break;
-            case 1:
+            case gum:
                 description.text = "A piece of gum I chewed on for a few years until my wife said I should be smoking cigarettes instead. It is REALLY sticky, but I am greasy enough that it doesn't stick onto me.";
                 break;
-            case 2:
+            case toenail:
                 description.text = "I had to clip this sucker after I saw that my big toe was starting to grow its own eco system. I forgot how long it was...";
                 break;
-            case 3:
+            case shoestring:
                 description.text = "I KNEW that cat made off with this! It's long enough to use as a makeshift rope.";
                 break;
-            case 4:
+            case remote:
                 description.text = "Ol' reliable...as long as it has a working set of batteries.";
                 break;
-            case 5:
+            case gloves:
                 description.text = "My wife insists that I use these when doing dirty work, or else she'll get the spoon out again.";
                 break;
-            case 6:
+            case battery:
                 description.text = "These only have enough juice in them to last for a few more seconds, so I will need to pick'em all up and turn on the TV fast...which is why you probably shouldn't be reading this right now.";
                 break;
         }
