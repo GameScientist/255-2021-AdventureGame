b0048b4336d8d0e5cc41a3fe6ce483494bdf7899
Tyler Kortge <tylerkortge@charter.net>
2021-01-29 10:22:30 -0500

    Resized GUI

---
+++ Assets/Scripts/InGameGUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameGUI : MonoBehaviour
+{
+    public Transform observe;
+    public Transform gum;
+    public Transform toenail;
+    public Transform shoelace;
+    public Transform TVRemote;
+    public Transform rubberGloves;
+    public Transform batttery1;
+    public Transform batttery2;
+    public Transform batttery3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
