129d3d9d52c612c81799d4f3d6ba08ee1c61748a
Tyler Kortge <tylerkortge@charter.net>
2021-01-30 16:26:59 -0500

    Made the equip badge have an image for each item.

--- Assets/Scripts/ItemSwitching.cs
+++ Assets/Scripts/ItemSwitching.cs
@@ -6,6 +6,17 @@ using UnityEngine.UI;
 public class ItemSwitching : MonoBehaviour
 {
     public static ItemSwitching _main;
+    public Transform image;
+
+    // images
+    RawImage rawImage;
+    public Texture none;
+    public Texture gum;
+    public Texture toenail;
+    public Texture shoelace;
+    public Texture remote;
+    public Texture gloves;
+    public Texture battery;
 
     public static ItemSwitching main
     {
@@ -36,24 +47,38 @@ public class ItemSwitching : MonoBehaviour
         {
             case 0:
                 item.text = "None";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = gum;
                 break;
             case 1:
                 item.text = "Gum";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = gum;
                 break;
             case 2:
                 item.text = "Toenail";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = toenail;
                 break;
             case 3:
                 item.text = "Shoelace";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = shoelace;
                 break;
             case 4:
                 item.text = "Remote";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = remote;
                 break;
             case 5:
                 item.text = "Gloves";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = gloves;
                 break;
             case 6:
                 item.text = "Battery";
+                rawImage = GetComponent<RawImage>();
+                rawImage.texture = battery;
                 break;
         }
     }
